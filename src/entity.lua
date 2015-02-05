073784b9e95c5b72d4e5a8a75a38dd7265377f58
LK <li289841680@gmail.com>
2015-02-06 00:20:45 +0800

    ADD: idle

--- src/entity.lua
+++ src/entity.lua
@@ -14,6 +14,8 @@ Entity = class("Entity",
 
 Entity.__index = Entity
 
+local ANIMATE_TYPE = {idle=0, run=1, attack=2, hurt=3, die=4}
+
 local function onNodeEvent(tag)
 	if tag == "exit" then
 		print("xxx")
@@ -36,67 +38,47 @@ function Entity:getRunAnimationName(dir)
 	return self.name .. string.format('-run-%d', dir)
 end
 
-function Entity:createRunAnimationFrames()
-	local spriteFrameCache = cc.SpriteFrameCache:getInstance()
-	-- local animationCache = cc.AnimationCache:getInstance()
-    runAnimationFrames = {}
-    for _, dir in pairs(Direction) do
-        local frames = {}
-        for i = 0, 7 do
-            table.insert(frames, spriteFrameCache:getSpriteFrame(self.name .. string.format("-run-%d%d.tga", dir, i)))
-        end
-        -- local runAnimation = cc.Animation:createWithSpriteFrames(frames, delay)
-        -- animationCache:addAnimation(runAnimation, self:getRunAnimationName(dir))
-        runAnimationFrames[dir] = frames
-    end
-
-    return runAnimationFrames
-end
-
-function Entity:createAttackAnimationFrames()
-    local spriteFrameCache = cc.SpriteFrameCache:getInstance()
-    -- local animationCache = cc.AnimationCache:getInstance()
-    local attackAnimationFrames = {}
-    for _, dir in pairs(DiagDirection) do
-        local frames = {}
-        for i = 0, 15 do
-            table.insert(frames, spriteFrameCache:getSpriteFrame(self.name .. string.format("-skill1-%d%d.tga", dir, i)))
-        end
-        -- local runAnimation = cc.Animation:createWithSpriteFrames(frames, delay)
-        -- animationCache:addAnimation(runAnimation, self:getRunAnimationName(dir))
-        attackAnimationFrames[dir] = frames
-    end
-
-    return attackAnimationFrames
-end
-
-function Entity:createHitAnimationFrames()
-    local spriteFrameCache = cc.SpriteFrameCache:getInstance()
-    local frames = {}
-    for _, dir in pairs(DiagDirection) do
-        frames[dir] = {}
-        for i = 0, 1 do 
-            table.insert(frames[dir], spriteFrameCache:getSpriteFrame("bgj" .. string.format("-hit-%d%d.tga", dir, i)))
-        end
-    end
-    return frames
-end
-
 function Entity:createHitEffectAnimationFrames()
 end
 
-function Entity:createDyingAnimationFrames()
+function Entity:createAnimationFrames(isFullDir, prefix, num)
     local spriteFrameCache = cc.SpriteFrameCache:getInstance()
     local frames = {}
-    for _, dir in pairs(DiagDirection) do
+
+    local directions
+    if isFullDir then
+        directions = Direction
+    else
+        directions = DiagDirection
+    end
+
+    for _, dir in pairs(directions) do
         frames[dir] = {}
-        for i = 0, 9 do 
-            table.insert(frames[dir], spriteFrameCache:getSpriteFrame("bgj" .. string.format("-die-%d%d.tga", dir, i)))
+        for i = 0, num-1 do 
+            local name = string.format("%s-%s-%d%d.tga", self.name, prefix, dir, i)
+            table.insert(frames[dir], spriteFrameCache:getSpriteFrame(name))
         end
     end
     return frames
 end
 
+function Entity:playAnimation(frames, callback, isFullDir, dt)
+    local dir
+    if isFullDir then
+        dir = self.dir
+    else
+        dir = FullToDiagDir[self.dir]
+    end
+
+    local animate = cc.Animate:create(cc.Animation:createWithSpriteFrames(frames[dir], dt))
+    if callback ~= nil then
+        local callFunc = cc.CallFunc:create(callback)
+        local seq = cc.Sequence:create(animate, callFunc)
+        self:runAction(seq)
+    else
+        self:runAction(animate)
+    end
+end
 
 function Entity:setStandDirection(dir)
 	local spriteFrameCache = cc.SpriteFrameCache:getInstance()
@@ -111,7 +93,7 @@ function Entity:stopRuning()
 	self:stopActionByTag(self.runActionTag)
 	self:stopActionByTag(self.runAnimateTag)
     self:setStandDirection(self.dir)
-    self.status = Status.idle
+    self:setStatus(Status.idle)
 end
 
 function Entity:_run(path, idx, cb_end, dir)
@@ -168,7 +150,7 @@ end
 function Entity:runPath(path, cb_end, dir)
 	self:stopActionByTag(self.runActionTag)	
 
-    self.status = Status.run
+    self:setStatus(Status.run)
 	self:_run(path, 1, cb_end, dir)
 end
 
@@ -181,19 +163,13 @@ function Entity:runOneStep(p, cb, dir)
         if cb ~= nil then
             cb()
         end
-        self.status = Status.idle
+        self:setStatus(Status.idle)
     end
     
     self:runPath({p}, cb_end, dir)
 end
 
 function Entity:releaseCache()
-	-- local spriteFrameCache = cc.SpriteFrameCache:getInstance()
-	-- spriteFrameCache:removeSpriteFramesFromFile(self.texturePlist)
-
-	-- for _, dir in pairs(Direction) do
- --        animationCache:removeAnimation(self:getRunAnimationName(dir))
- --    end
 end
 
 function Entity:tryAttack()
@@ -202,22 +178,36 @@ function Entity:tryAttack()
     end
 
 	self:stopRuning()
-    self.status = Status.attack
-	local animate = cc.Animate:create(cc.Animation:createWithSpriteFrames(self.attackAnimationFrames[FullToDiagDir[self.dir]], self.runAnimDelay / 2))
+    self:setStatus(Status.attack)
 	local cb = function ()
         self:setStandDirection(self.dir)
-        self.status = Status.idle
+        self:setStatus(Status.idle)
 	end
-	local callFunc = cc.CallFunc:create(cb)
-	local seq = cc.Sequence:create(animate, callFunc)
-	self:runAction(seq)
+
+    self:playAnimation(self.attackAnimationFrames, cb, false, self.runAnimDelay * 0.5)
 
     return true
 end
 
+function Entity:setStatus(status)
+    if status == Status.idle then
+        if self:getActionByTag(self.idleActionTag) == nil then
+            local delayTime = cc.DelayTime:create(3)
+            local repeatForever = cc.RepeatForever:create(cc.Animate:create(
+                cc.Animation:createWithSpriteFrames(self.idleAnimationFrames[self.dir], self.runAnimDelay)))
+            local seq = cc.Sequence:create(delayTime, repeatForever)
+            seq:setTag(self.idleActionTag)
+            self:runAction(seq)
+        end
+    else
+        self:stopActionByTag(self.idleActionTag)
+    end
+    self.status = status
+end
+
 function Entity:onHurt(atk)
     -- self:stopRuning()
-    self.status = Status.attack
+    self:setStatus(Status.hurt)
     local nhurt = atk - self.def
     self.hp = self.hp - nhurt
     
@@ -236,25 +226,19 @@ function Entity:onHurt(atk)
 
     -- 死亡
     if self.hp <= 0 then
-        local animate = cc.Animate:create(cc.Animation:createWithSpriteFrames(self.dyingAnimationFrames[FullToDiagDir[self.dir]], self.runAnimDelay))
-        -- local animate = cc.Animate:create(cc.Animation:createWithSpriteFrames(self.attackAnimationFrames[self.dir], self.runAnimDelay))
         local cb = function ()
-            self.status = Status.die
             self:removeFromParent(true)
         end
-        local callFunc = cc.CallFunc:create(cb)
-        local seq = cc.Sequence:create(animate, callFunc)
-        self:runAction(seq)
+        self:setStatus(Status.die)
+        self:playAnimation(self.dyingAnimationFrames, cb, false, self.runAnimDelay)
     else
-        local animate = cc.Animate:create(cc.Animation:createWithSpriteFrames(self.hitAnimationFrames[FullToDiagDir[self.dir]], self.runAnimDelay * 2))
-        -- local animate = cc.Animate:create(cc.Animation:createWithSpriteFrames(self.attackAnimationFrames[self.dir], self.runAnimDelay))
         local cb = function ()
-            self.status = Status.idle
-            self:setStandDirection(self.dir)
+            if self.status == Status.hurt then
+                self:setStatus(Status.idle)
+                self:setStandDirection(self.dir)
+            end
         end
-        local callFunc = cc.CallFunc:create(cb)
-        local seq = cc.Sequence:create(animate, callFunc)
-        self:runAction(seq)
+        self:playAnimation(self.hitAnimationFrames, cb, false, self.runAnimDelay*2)
     end
 end
 
@@ -265,6 +249,7 @@ function Entity:init(name, camp)
 	self.runAnimDelay = 0.1
 	self.runActionTag = 10
 	self.runAnimateTag = 11
+    self.idleActionTag = 12
 	self.texturePlist = self.name .. ".plist"
     self.status = Status.idle
     self.camp = camp
@@ -276,15 +261,19 @@ function Entity:init(name, camp)
 	local spriteFrameCache = cc.SpriteFrameCache:getInstance()
 	spriteFrameCache:addSpriteFrames(self.texturePlist)
 
-	self.runAnimationFrames = self:createRunAnimationFrames()
+	self.runAnimationFrames = self:createAnimationFrames(true, 'run', 8)
 
-	self.attackAnimationFrames = self:createAttackAnimationFrames()
+	self.attackAnimationFrames = self:createAnimationFrames(false, 'skill1', 16)
 
-    self.hitAnimationFrames = self:createHitAnimationFrames()
+    self.hitAnimationFrames = self:createAnimationFrames(false, 'hit', 2)
 
-    self.dyingAnimationFrames = self:createDyingAnimationFrames()
+    self.dyingAnimationFrames = self:createAnimationFrames(false, 'die', 10)
+
+    self.idleAnimationFrames = self:createAnimationFrames(true, 'stand', 8)
 
 	self:setStandDirection(Direction.S)
+
+    self:setStatus(Status.idle)
 
 	self:setAnchorPoint(cc.p(0.5, 0))
 

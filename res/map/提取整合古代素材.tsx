6ea43e1ee6b8e0d3c7931191daf1ea68f3856dc8
likang7 <likang7@foxmail.com>
2015-02-10 01:04:12 +0800

    添加图片资源

--- res/map/提取整合古代素材.tsx
+++ res/map/提取整合古代素材.tsx
@@ -1,6 +1,6 @@
-<?xml version="1.0" encoding="UTF-8"?>
+﻿<?xml version="1.0" encoding="UTF-8"?>
 <tileset name="提取整合古代素材" tilewidth="32" tileheight="32">
- <image source="pic2487_古代地图元件素材/全新古代素材整合/提取整合古代素材.png" width="256" height="6963"/>
+ <image source="提取整合古代素材.png" width="256" height="6963"/>
  <terraintypes>
   <terrain name="草地" tile="-1"/>
  </terraintypes>

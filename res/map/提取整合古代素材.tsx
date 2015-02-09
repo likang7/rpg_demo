37ca3ad8a4f04a54ef85d2eb61c00f0404bbf56b
likang7 <likang7@foxmail.com>
2015-02-10 00:44:12 +0800

    地图图块资源

---
+++ res/map/提取整合古代素材.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="提取整合古代素材" tilewidth="32" tileheight="32">
+ <image source="pic2487_古代地图元件素材/全新古代素材整合/提取整合古代素材.png" width="256" height="6963"/>
+ <terraintypes>
+  <terrain name="草地" tile="-1"/>
+ </terraintypes>
+ <tile id="640" terrain="0,0,0,0"/>
+ <tile id="641" terrain="0,0,0,0"/>
+ <tile id="642" terrain="0,0,0,0"/>
+ <tile id="643" terrain="0,0,0,0"/>
+ <tile id="644" terrain="0,0,0,0"/>
+</tileset>
